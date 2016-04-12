18035e33f300ac7349d1c236dc188ff77a0badd7
Joygeorgek <joyg@orioninc.com>
2016-04-11 22:21:43 -0400

    Initial checkin

---
+++ src/script/9-app.js
@@ -0,0 +1,35 @@
+﻿var myApp = angular.module('myApp', [
+    'datatables',
+    'chart.js',
+    'picardy.fontawesome',
+    'myControllers',
+    'ngRoute'
+]);
+
+myApp.config(['$routeProvider',
+function ($routeProvider) {
+    $routeProvider.
+    when('/myteam', {
+        templateUrl: '9-myteam.html',
+        controller: 'myTeamCtrl'
+    }).
+    when('/teams', {
+        templateUrl: '9-teams.html',
+        controller: 'myCtrl'
+    }).
+    when('/players', {
+        templateUrl: '9-players.html',
+        controller: 'myCtrl'
+    }).
+    when('/capacity', {
+        templateUrl: '9-capacity.html',
+        controller: 'myCtrl'
+    }).
+    when('/pick', {
+        templateUrl: '9-pick.html',
+        controller: 'myCtrl'
+    }).
+    otherwise({
+        redirectTo: '/teams'
+    });
+}]);
