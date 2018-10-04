4057dc4b80e9ac1f8cca68182e5ef53a56fd7da2
joymon <joymon@gmail.com>
2018-10-03 20:31:47 -0400

    Arranged folder and added new code

---
+++ courses/udacity/Software Debugging/htmlExtract.py
@@ -0,0 +1,24 @@
+def remove_html_markup(s):
+    print("called")
+    tag = False
+    quote = False
+    out = ""
+
+    for c in s:
+        assert not tag
+        if c == '<' and not quote:
+            tag = True
+            print("stripped")
+        elif c == '>' and not quote:
+            tag = False
+        elif c == '"' or c == "'" and tag:
+            quote = not quote
+        elif not tag:
+            out = out + c
+
+    return out
+
+
+inp = "<f>'PhillyDotNet-CodeCamp2016-ModernWeb'</f></f>"
+out = remove_html_markup(inp)
+print(out)
---
+++ courses/udacity/Software Debugging/readme.md
@@ -0,0 +1,3 @@
+Code what is tried for Course
+
+[Software debugging](https://classroom.udacity.com/courses/cs259)
